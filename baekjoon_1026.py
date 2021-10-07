7565e0d2ee2a3b8a5eaa0c940a88ff94a57b5164
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-07 15:34:09 +0900

    Update baekjoon_1026.py

--- baekjoon_1026.py
+++ baekjoon_1026.py
@@ -1,6 +1,11 @@
+temp = 0
 n = int(input())
+
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
+a.sort(), b.sort(), b.reverse()
 
-a = a.sort()
-print(a)
+for i in range(n):
+    temp += a[i] * b[i]
+
+print(temp)
