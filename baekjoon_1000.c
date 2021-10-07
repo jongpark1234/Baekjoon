926becb70dd432213a4959cc1ab7531bd6f658cd
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-07 19:50:31 +0900

    Update

--- baekjoon_1000.c
+++
@@ -1,8 +0,0 @@
-#pragma warning(disable:4996)
-#include <stdio.h>
-
-int main() {
-	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%d", a + b);
-}
---
+++ baekjoon_1330.py
@@ -0,0 +1,4 @@
+a, b = input().split()
+if (int(a) > int(b)): print(">")
+elif (int(a) < int(b)): print("<")
+else: print("==")
