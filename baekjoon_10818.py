e0d99e5358ddbf64f0eee22017c62c042aa28dec
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-07 23:29:42 +0900

    Update baekjoon_10818.py

--- baekjoon_10818.py
+++ baekjoon_10818.py
@@ -1,12 +1,5 @@
 n = int(input())
 numlist = list(map(int, input().split()))
-max = 0
-min = 1000001
-for i in range(len(numlist)):
-    if (numlist[i] > max):
-        max = numlist[i]
-for j in range(len(numlist)):
-    if (numlist[j] < min):
-        min = numlist[j]
-
-print(min, max)
+numlist.sort()
+maxnum, minnum = numlist[len(numlist) - 1], numlist[0]
+print(minnum, maxnum)
