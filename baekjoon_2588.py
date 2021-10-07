e547b2970900387dfd3b9fbb80e5190dc8f3c577
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-07 19:10:13 +0900

    Update baekjoon_2588.py

--- baekjoon_2588.py
+++ baekjoon_2588.py
@@ -1,9 +1,6 @@
 a = int(input())
 b = int(input())
 c = list(map(int, str(b)))
-
-for i in range(len(str(b))):
-    print(a * c[len(c) - 1])
-    c.pop()
+for i in range(len(str(b))): print(a * c[len(c) - 1]), c.pop()
 print(a * b)
 
