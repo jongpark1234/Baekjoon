fd476fe4d138ebb8bf9f25e8067c0fa33859d4c3
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-09 01:45:57 +0900

    Create baekjoon_8958.py

---
+++ baekjoon_8958.py
@@ -0,0 +1,15 @@
+n = int(input())
+for i in range(n):
+    score = 0
+    temp = 0
+    b = input()
+    OXlist = list(map(str, b))
+    for j in range(len(OXlist)):
+        if (OXlist[j] == 'O'):
+            temp += 1
+            score += temp
+        else:
+            temp = 0
+    print(score)
+    
+    
