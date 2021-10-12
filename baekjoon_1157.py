90078b34906df54bb5c87e10da1615c62362b1a6
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-12 14:56:06 +0900

    Update baekjoon_1157.py

--- baekjoon_1157.py
+++ baekjoon_1157.py
@@ -4,5 +4,7 @@ count_list = []
 for i in string_set:   
     count = string.count(i)
     count_list.append(count)
-if count_list.count(max(count_list)) > 1:   print("?")
-else:   print(string_set[count_list.index(max(count_list))])
+if count_list.count(max(count_list)) > 1:   
+    print("?")
+else:   
+    print(string_set[count_list.index(max(count_list))])
