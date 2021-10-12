fd6c7727daacc8ef849963230a77d8958d1a7e32
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-12 17:33:23 +0900

    Update baekjoon_10809.py

--- baekjoon_10809.py
+++ baekjoon_10809.py
@@ -1,3 +1,10 @@
 from sys import *
-
 s = stdin.readline()
+alist = []
+for i in range(26):
+    alist.append(-1)
+for j in range(len(s)):
+    print(ord(s[j]) - 97)
+    # alist[ord(s[j]) - 97] = j
+
+print(alist)
