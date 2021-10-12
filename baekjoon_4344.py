bd468fd138c24958a3261c476f4064d4d4a4df4e
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-12 13:20:41 +0900

    Update baekjoon_4344.py

--- baekjoon_4344.py
+++ baekjoon_4344.py
@@ -1,6 +1,12 @@
 n = int(input())
-
+upper = 0
+temp = 0
 for i in range(n):
     t = list(map(int, input().split()))
-    print(t)
-    print("{:.3f}".format(round((2/5) * 100, 4)))
+    temp = (sum(t) - t[0]) / t[0]
+    i = 1
+    for i in range(t[1:]):
+        if (t[i] > temp):
+            upper += 1
+    print("{:.3f}%".format(round((upper/t[0]) * 100, 4)))
+    upper = 0
