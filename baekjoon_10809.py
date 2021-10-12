bdeafc2defe25c3fcd99c29117b762ee3cb3a69e
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-12 19:41:19 +0900

    Update baekjoon_10809.py

--- baekjoon_10809.py
+++ baekjoon_10809.py
@@ -1,10 +1,14 @@
-from sys import *
-s = stdin.readline()
+s = input()
+save = []
 alist = []
 for i in range(26):
     alist.append(-1)
 for j in range(len(s)):
-    print(ord(s[j]) - 97)
-    # alist[ord(s[j]) - 97] = j
+    temp = ord(s[j]) - 97
+    if save.count(s[j]) == 0:
+        alist[temp] = j
+        save.append(s[j])
+for k in range(len(alist)):
+    print(alist[k], end=" ")
 
-print(alist)
+print(save)
