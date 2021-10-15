135a6d4d36e2e7483929734b3fd15f93c5aa3f43
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-15 13:33:11 +0900

    Create baekjoon_10828.py

---
+++ baekjoon_10828.py
@@ -0,0 +1,25 @@
+import sys
+n = int(sys.stdin.readline())
+stack = []
+for i in range(n):
+    command = sys.stdin.readline().split()
+    if command[0] == 'push':
+        stack.append(command[1])
+    elif command[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[len(stack) - 1])
+            stack.pop()
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[len(stack) - 1])
