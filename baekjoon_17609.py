0ace88bdbf27d48fd18dccd5b84cf3e0fddbae12
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-15 11:06:36 +0900

    Update baekjoon_17609.py

--- baekjoon_17609.py
+++ baekjoon_17609.py
@@ -1,34 +1,24 @@
-import sys
-t = int(sys.stdin.readline())
+from sys import *
+def pseudoPalindrome(s, l, r):
+    while l < r:
+        if s[l] == s[r]:
+            l += 1
+            r -= 1
+        else: return 0
+    return 1
+def palindrome(s, l, r):
+    while l < r:
+        if s[l] == s[r]:
+            l += 1
+            r -= 1
+        else:
+            fs = pseudoPalindrome(s, l + 1, r)
+            ss = pseudoPalindrome(s, l, r - 1)
+            if (fs == 1) or (ss == 1): return 1
+            else: return 2
+    return 0
+
+t = int(stdin.readline())
 for i in range(t):
-    temp = 0
-    count = 0
-    string = sys.stdin.readline().rstrip()
-    middle = int(len(list(string)) / 2)
-    fs, ss = list(string[:middle]), list(string[middle:])
-    ss.reverse()
-    if (len(fs) != (len(ss))): # When String length is odd
-        if (fs == ss[:-1]): # same
-            pass
-        else:
-            for j in range(len(fs)):
-                if count == 2:
-                    break
-                elif (fs[j] == ss[j + temp]):
-                    pass
-                else:
-                    temp += 1
-                    j -= 1
-                    count += 1
-    else: # When String length is even
-        if (fs == ss): # same
-            pass
-        else:
-            for j in range(len(fs)):
-                if count == 2:
-                    break
-                elif (fs[j] == ss[j]):
-                    pass
-                else:
-                    count += 1
-    print(count)
+    string = list(stdin.readline().rstrip())
+    print(palindrome(string, 0, len(string) - 1))
