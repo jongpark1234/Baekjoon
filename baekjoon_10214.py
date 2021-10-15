ba4d93fac07d4181160c4cf03d487ce3939e7a5f
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-15 12:12:50 +0900

    Create baekjoon_10214.py

---
+++ baekjoon_10214.py
@@ -0,0 +1,17 @@
+import sys
+t = int(sys.stdin.readline())
+yonsei = 0
+korea = 0
+for i in range(t):
+    korea, yonsei = 0, 0
+    for j in range(9):
+        y, k = map(int, sys.stdin.readline().split())
+        yonsei += y
+        korea += k
+    if yonsei > korea:
+        print("Yonsei")
+    elif korea > yonsei:
+        print("Korea")
+    else:
+        print("Draw")
+    
