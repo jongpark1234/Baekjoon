e130c1f94770abe7a61ff762b32e5babfef4df8f
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-17 13:28:22 +0900

    수학은 체육과목 입니다 2
    ㆍ수학

---
+++ baekjoon_17362.py
@@ -0,0 +1,12 @@
+n = int(input()) % 8
+if (n == 1):
+    print(1)
+elif (n == 2 or n == 0):
+    print(2)
+elif (n == 3 or n == 7):
+    print(3)
+elif (n == 4 or n == 6):
+    print(4)
+elif (n == 5):
+    print(5)
+    
