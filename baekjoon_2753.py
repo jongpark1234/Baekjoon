fbc2871bb03863aa10e6716f06e94cfabdfe3424
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-18 11:24:20 +0900

    윤년
    ㆍ수학
    ㆍ구현

--- baekjoon_2753.py
+++ baekjoon_2753.py
@@ -1,5 +1,4 @@
 year = int(input())
 leap = 0
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    leap = 1
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0): leap = 1
 print(leap)
