727be6f55c7592bf20bde598d0e506733c9b50ea
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-20 12:16:52 +0900

    큐
    ㆍ자료 구조
    ㆍ큐

---
+++ baekjoon_10845.py
@@ -0,0 +1,30 @@
+from sys import *
+n = int(stdin.readline())
+queue = []
+for _ in range(n):
+    command = stdin.readline().split()
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+            del queue[0]
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[len(queue) - 1])
