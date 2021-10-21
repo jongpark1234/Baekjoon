5a8e425432ed63b59594175f81acd073cae786a0
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-21 14:44:54 +0900

    큐 2
    ㆍ자료 구조
    ㆍ큐

---
+++ baekjoon_18258.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+n = int(sys.stdin.readline())
+queue = deque([])
+for i in range(n):
+    command = sys.stdin.readline().rstrip().split()
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft())
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
