a57eed80396359471abebd1df775bb7bc2c5d536
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-28 20:33:46 +0900

    가장 많은 글자
    ㆍ구현
    ㆍ문자열

---
+++ baekjoon_1371.py
@@ -0,0 +1,8 @@
+import sys
+wordlist, s = [x for x in range(26)], sys.stdin.read()
+for i in s:
+    if i.islower():
+        wordlist[ord(i) - 97] += 1
+for i in range(26):
+    if wordlist[i] == max(wordlist):
+        print(chr(i + 97), end='')
