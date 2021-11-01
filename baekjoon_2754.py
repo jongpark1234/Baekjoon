4ac1c8775d407e0c076d40d2a082a03d8ed80356
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-11-01 11:06:07 +0900

    학점계산
    ㆍ수학
    ㆍ구현
    ㆍ문자열

---
+++ baekjoon_2754.py
@@ -0,0 +1,4 @@
+grade = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D+', 'D0', 'D-', 'F']
+rating = [4.3, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0.0]
+Cgrade = input()
+print(rating[grade.index(Cgrade)])
