c29968b92b65949dc448b054799c0a80914f5885
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-11-06 18:04:44 +0900

    숫자놀이
    ㆍ수학
    ㆍ문자열
    ㆍ정렬

---
+++ baekjoon_1775.py
@@ -0,0 +1,15 @@
+keyword = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+dictlist = []
+m, n = map(int, input().split())
+numlist = [i for i in range(m, n + 1)]
+print(numlist)
+for i in numlist:
+    word = ''
+    for j in str(i):
+        word += keyword[int(j)]
+    dictlist.append([word, i])
+dictlist.sort()
+for i in range(len(dictlist)):
+    if i % 10 == 0 and i != 0:
+        print()
+    print(dictlist[i][1], end=' ')
