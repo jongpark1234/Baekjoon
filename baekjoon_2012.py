a98b4cf3a9c4a5561f04397fee4a5b5bfe0d1550
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-11-09 10:45:58 +0900

    등수 매기기
    ㆍ그리디 알고리즘
    ㆍ정렬

--- baekjoon_2012.py
+++ baekjoon_2012.py
@@ -1,5 +1,6 @@
+from sys import stdin
 complain = 0
-numlist = sorted([int(input()) for i in range(int(input()))])
+numlist = sorted([int(stdin.readline()) for i in range(int(stdin.readline()))])
 for i in range(len(numlist)):
     complain += abs(numlist[i] - (i + 1))
 print(complain)
