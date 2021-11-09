ea4dcf22f6e09d051603dae9d1e3bfd08f0e99ff
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-11-09 11:19:13 +0900

    팔
    ㆍ수학
    ㆍ그리디 알고리즘

---
+++ baekjoon_1105.py
@@ -0,0 +1,14 @@
+l, r = input().split()
+eight = 0
+if len(l) != len(r) or l[0] != r[0]:
+    print(0)
+else:
+    if l[0] == '8':
+        eight += 1
+    for i in range(1, len(l)):
+        if l[i] != r[i]:
+            break
+        else:
+            if l[i] == '8':
+                eight += 1
+print(eight)
