fdaf0ae920f97e2c753c319047c473118e9b5881
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-11-13 01:44:23 +0900

    문자열 집합
    ㆍ자료 구조
    ㆍ문자열
    ㆍ트리
    ㆍ트리를 사용한 집합과 맵
    ㆍ트라이

--- baekjoon_14425.py
+++ baekjoon_14425.py
@@ -3,7 +3,7 @@ count = 0
 n, m = map(int, stdin.readline().split())
 s = [stdin.readline().rstrip() for i in range(n)]
 strlist = [stdin.readline().rstrip() for i in range(m)]
-for i in s:
-    if i in strlist:
+for i in strlist:
+    if i in s:
         count += 1
 print(count)
