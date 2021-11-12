290c07fa315d50ba607c64d30b252acd70d044c4
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-11-12 10:07:50 +0900

    큰 수 만들기
    ㆍ그리디 알고리즘
    ㆍ정렬

--- baekjoon_16496.py
+++ baekjoon_16496.py
@@ -2,4 +2,4 @@ from functools import cmp_to_key
 from sys import stdin
 n = int(stdin.readline())
 numlist = sorted(stdin.readline().rstrip().split(), key = cmp_to_key(lambda x, y: 1 if int(x + y) < int(y + x) else -1))
-print(''.join(numlist))
+print(int(''.join(numlist)))
