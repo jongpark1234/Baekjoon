abdc2a2756491df3cac10849132defdda5314b67
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-11-15 17:08:33 +0900

    모르고리즘 회장님 추천 받습니다
    ㆍ구현
    ㆍ문자열

--- baekjoon_20124.py
+++ baekjoon_20124.py
@@ -1,2 +1,3 @@
-namelist = [list(input().split()) for _ in range(int(input()))]
+from sys import stdin
+namelist = [list(stdin.readline().split()) for _ in range(int(stdin.readline()))]
 print(sorted(namelist, key = lambda x : (-int(x[1]), x[0]))[0][0])
