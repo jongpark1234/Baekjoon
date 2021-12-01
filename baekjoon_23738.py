efd05cb3a58169ef5f0aaf4b0d670205d72a438d
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-12-01 13:12:41 +0900

    Ресторан
    ㆍ구현
    ㆍ문자열

---
+++ baekjoon_23738.py
@@ -0,0 +1,5 @@
+alphadict = {'B':'v', 'E':'ye', 'H':'n', 'P':'r', 'C':'s', 'Y':'u', 'X':'h', 'A':'a', 'K':'k', 'M':'m', 'O':'o', 'T':'t'}
+string = ''
+for i in input():
+    string += alphadict[i]
+print(string)
