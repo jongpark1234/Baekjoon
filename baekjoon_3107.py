42cc5205fedc25d880b418499687354c77cd8f18
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-12-03 09:19:02 +0900

    IPv6
    ㆍ구현
    ㆍ문자열

---
+++ baekjoon_3107.py
@@ -0,0 +1,13 @@
+s = input()
+s = s.replace('::', ':^:')
+ip = s.split(':')
+if '' in ip:
+    ip.remove('')
+for i in range(len(ip)):
+    if ip[i] != '^':
+        ip[i] = ip[i].zfill(4)
+if '^' in ip:
+    for i in range(9 - len(ip)):
+        ip.insert(ip.index('^'), '0000')
+    ip.remove('^')
+print(':'.join(ip))
