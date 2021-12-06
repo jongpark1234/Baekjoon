941ff8cc3eec7e8981cca91d19edd2898538b5be
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-12-06 22:21:24 +0900

    궁합 쌍 찾기
    ㆍ브루트포스 알고리즘
    ㆍ정렬

---
+++ baekjoon_14911.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+from sys import stdin
+numlist = list(combinations(list(map(int, stdin.readline().split())), 2))
+result = []
+n = int(stdin.readline())
+for i in numlist:
+    if sum(i) == n:
+        result.append(i)
+result = list(set(result))
+print(result)
+for i in result:
+    print(*i)
+print(len(result))
