287c58bddd42e44646b84530c1f4d33ebc7db725
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-12-06 19:28:33 +0900

    이진수 연산
    ㆍ문자열

--- baekjoon_12813.py
+++ baekjoon_12813.py
@@ -1,15 +1,15 @@
 a = int(input(), 2)
 b = int(input(), 2)
-print(f'{int(bin(a & b)[2:]):010d}')
-print(f'{int(bin(a | b)[2:]):010d}')
-print(f'{int(bin(a ^ b)[2:]):010d}')
-for i in bin(a)[2:].zfill(10):
+print(f'{int(bin(a & b)[2:]):0100000d}')
+print(f'{int(bin(a | b)[2:]):0100000d}')
+print(f'{int(bin(a ^ b)[2:]):0100000d}')
+for i in bin(a)[2:].zfill(100000):
     if i == '0':
         print('1', end='')
     else:
         print('0', end='')
 print()
-for i in bin(b)[2:].zfill(10):
+for i in bin(b)[2:].zfill(100000):
     if i == '0':
         print('1', end='')
     else:
