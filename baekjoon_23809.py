c412537adfdc2ff3f90bde78b78dabe0deb7b65e
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-12-11 00:28:28 +0900

    골뱅이 찍기 - 돌아간 ㅈ
    ㆍ구현

---
+++ baekjoon_23809.py
@@ -0,0 +1,11 @@
+n = int(input())
+for _ in range(n):
+    print('@' * n + ' ' * n * 3 + '@' * n)
+for _ in range(n):
+    print('@' * n + ' ' * n * 2 + '@' * n)
+for _ in range(n):
+    print('@' * n * 3)
+for _ in range(n):
+    print('@' * n + ' ' * n * 2 + '@' * n)
+for _ in range(n):
+    print('@' * n + ' ' * n * 3 + '@' * n)
