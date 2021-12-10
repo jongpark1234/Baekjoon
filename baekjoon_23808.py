817df51c609304b16003f3a9185d78dc0486631c
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-12-11 00:21:58 +0900

    골뱅이 찍기 - ㅂ
    ㆍ구현

---
+++ baekjoon_23808.py
@@ -0,0 +1,9 @@
+n = int(input())
+for _ in range(n * 2):
+    print('@' * n + ' ' * n * 3 + '@' * n)
+for _ in range(n):
+    print('@' * n * 5)
+for _ in range(n):
+    print('@' * n + ' ' * n * 3 + '@' * n)
+for _ in range(n):
+    print('@' * n * 5)
