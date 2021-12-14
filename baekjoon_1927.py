f6bac75c33e4ee23e0d8e13f6c22836e03af993c
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-12-14 19:12:42 +0900

    최소 힙
    ㆍ자료 구조
    ㆍ우선순위 큐

---
+++ baekjoon_1927.py
@@ -0,0 +1,12 @@
+from heapq import heappush, heappop
+from sys import stdin
+q = []
+for i in range(int(stdin.readline())):
+    x = int(stdin.readline().rstrip())
+    if x == 0:
+        if not len(q):
+            print(0)
+        else:
+            print(heappop(q))
+    else:
+        heappush(q, x)
