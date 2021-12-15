64dff45b6730d3f5bd794bbd7131b362c07ce63e
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-12-15 15:17:52 +0900

    용액
    ㆍ이분 탐색
    ㆍ두 포인터

---
+++ baekjoon_2467.py
@@ -0,0 +1,17 @@
+from sys import stdin, maxsize
+n = int(input())
+value = list(map(int, input().split()))
+distance = maxsize
+l, r = 0, n - 1
+while l < r:
+    solution = value[l] + value[r]
+    if abs(solution) < distance:
+        result = [value[l], value[r]]
+        distance = abs(solution)
+    if solution < 0:
+        l += 1
+    elif solution > 0:
+        r -= 1
+    else:
+        break
+print(*result)
