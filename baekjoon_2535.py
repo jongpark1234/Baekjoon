f116c371b646543130384a2ac86b103a8943ca54
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-01-14 02:32:54 +0900

    아시아 정보올림피아드
    ㆍ구현
    ㆍ정렬

--- baekjoon_2535.py
+++ baekjoon_2535.py
@@ -1,13 +1,17 @@
 from sys import stdin
-dic = {}
-students = []
+cnt = 0
+stu = []
+team = {}
 for i in range(int(stdin.readline())):
-    s = list(map(int, stdin.readline().split()))
-    students.append(s)
-    dic[s[0]] = 0
-students.sort(key = lambda x: -x[2])
-for i in range(3):
-    dic[students[i][0]] += 1
-    if dic[students[i][0]] == 3:
-        i += 1
-    print(students[i][0], students[i][1])
+    Input = list(map(int, stdin.readline().split()))
+    team[Input[0]] = 0
+    stu.append([*Input])
+stu.sort(key = lambda x: -x[2])
+for i in sorted(stu, key = lambda x: -x[2]):
+    team[i[0]] += 1
+    if team[i[0]] > 2:
+        continue
+    print(i[0], i[1])
+    cnt += 1
+    if cnt == 3:
+        break
