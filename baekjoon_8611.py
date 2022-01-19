62db8be51300f928a251cceadaaf171201c50a55
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-01-19 16:43:17 +0900

    팰린드롬 숫자
    ㆍ수학
    ㆍ구현

---
+++ baekjoon_8611.py
@@ -0,0 +1,15 @@
+def solution(n, q):
+    rev_base = ''
+    while n > 0:
+        n, mod = divmod(n, q)
+        rev_base += str(mod)
+    return rev_base[::-1]
+n = int(input())
+palindrome = False
+for i in range(2, 11):
+    num = solution(n, i)
+    if num == num[::-1]:
+        palindrome = True
+        print(num)
+if not palindrome:
+    print('NIE')
