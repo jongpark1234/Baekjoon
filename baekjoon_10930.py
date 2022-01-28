77fea11e248c019ffda6edd270cfddd3e73fdfe5
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-01-28 20:22:27 +0900

    SHA-256
    ㆍ구현
    ㆍ문자열

--- baekjoon_10930.py
+++ baekjoon_10930.py
@@ -1,4 +1,2 @@
 import hashlib
-string_to_hash = input()
-hash_object = hashlib.sha256(str(string_to_hash).encode('utf-8'))
-print(hash_object.hexdigest())
+print(hashlib.sha256(input().encode()).hexdigest())
