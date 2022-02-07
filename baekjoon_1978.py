22ed929d12a93a1fba429aecd9acd81667ac9182
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-02-07 09:06:28 +0900

    소수 찾기
    ㆍ수학
    ㆍ정수론
    ㆍ소수 판정
    ㆍ에라토스테네스의 체

--- baekjoon_1978.py
+++ baekjoon_1978.py
@@ -1,12 +1,13 @@
+def isprime(num):
+    if num == 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+cnt = 0
 n = int(input())
-numlist = list(map(int, input().split()))
-count = 0
-for i in numlist:
-    wrong = 0
-    if i > 1:
-        for j in range(2, i):
-            if i % j == 0:
-                wrong += 1
-        if wrong == 0:
-            count += 1
-print(count)
+for i in map(int, input().split()):
+    if isprime(i):
+        cnt += 1
+print(cnt)
