2a6f3f5e0bd9364eaeb147ab01f713bf6cb32040
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-02-22 16:38:58 +0900

    A → B
    ㆍ그래프 이론
    ㆍ그리디 알고리즘
    ㆍ그래프 탐색
    ㆍ너비 우선 탐색

--- baekjoon_16953.py
+++ baekjoon_16953.py
@@ -1,16 +1,16 @@
 a, b = map(int, input().split())
-cnt = 1
+result = 1
 while True:
     if a == b:
         break
     elif (b % 2 and b % 10 != 1) or (a > b):
-        cnt = -1
+        result = -1
         break
     else:
         if b % 10 == 1:
             b //= 10
-            cnt += 1
+            result += 1
         else:
             b //= 2
-            cnt += 1
-print(cnt)
+            result += 1
+print(result)
