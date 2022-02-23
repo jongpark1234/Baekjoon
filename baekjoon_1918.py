21abeb6e81bce88e0f00153f05219e9daaad6ac6
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-02-23 17:24:19 +0900

    후위 표기식
    ㆍ자료 구조
    ㆍ스택

---
+++ baekjoon_1918.py
@@ -0,0 +1,24 @@
+Str = list(input())
+stack = []
+result = ''
+for i in Str:
+    if i.isalpha():
+        result += i
+    else:
+        if i == '(':
+            stack.append(i)
+        elif i == '*' or i == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                result += stack.pop()
+            stack.append(i)
+        elif i == '+' or i == '-':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.append(i)
+        elif i == ')':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.pop()
+while stack:
+    result += stack.pop()
+print(result)
