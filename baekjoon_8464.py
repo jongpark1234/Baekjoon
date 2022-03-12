95f5429ab08ba50c62178c3ba24dbcc295e8384c
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-03-13 00:43:36 +0900

    Non-Squarefree Numbers
    ㆍ수학
    ㆍ정수론
    ㆍ이분 탐색
    ㆍ포함 배제의 원리

---
+++ baekjoon_8464.py
@@ -0,0 +1,20 @@
+def squareNoNo(n):
+    p = 0
+    for i in range(1, int(n ** 0.5) + 1):
+        p += mobius[i] * (n // (i * i))
+    return p
+l, r = 0, 2000000000
+mobius = [0] * 1000001
+k = int(input())
+mobius[1] = 1
+for i in range(1, 1000001):
+    if mobius[i]:
+        for j in range(i * 2, 1000001, i):
+            mobius[j] -= mobius[i]
+while l < r - 1:
+    mid = (l + r) // 2
+    if mid - squareNoNo(mid) < k:
+        l = mid
+    else:
+        r = mid
+print(r)
