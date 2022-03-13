7ff6bd9cfce8fe4a7e6c8e02b28158857ae19306
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-03-14 01:11:39 +0900

    라면 사기 (Small)
    ㆍ그리디 알고리즘

---
+++ baekjoon_18185.py
@@ -0,0 +1,35 @@
+amount = 0
+i = 0
+b, c = 3, 2
+w = False
+n = int(input())
+A = list(map(int, input().split()))
+while i < n:
+    if i < n - 2 and A[i] and A[i + 1] and A[i + 2]:
+        if w:
+            s = min(A[i], A[i + 1])
+            A[i] -= s
+            A[i + 1] -= s
+            amount += (b + c) * s
+            w = False
+        else:
+            s = min(min(A[i], A[i + 1]), A[i + 2])
+            if A[i] < A[i + 1] and A[i + 1] > A[i + 2]:
+                s -= min(min(A[i], A[i + 2]), min(A[i + 1] - A[i], A[i + 1] - A[i + 2]))
+                w = True
+            A[i] -= s
+            A[i + 1] -= s
+            A[i + 2] -= s
+            amount += (b + c * 2) * s
+            continue
+    if i < n - 1 and A[i] and A[i + 1]:
+        s = min(A[i], A[i + 1])
+        A[i] -= s
+        A[i + 1] -= s
+        amount += (b + c) * s
+    if A[i]:
+        s = A[i]
+        A[i] = 0
+        amount += s * b
+    i += 1
+print(amount)
