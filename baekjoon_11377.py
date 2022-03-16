34dc20641705070e8f09adfbea9057ddd29c3901
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-03-16 16:03:59 +0900

    열혈강호 3
    ㆍ최대 유량
    ㆍ이분 매칭

---
+++ baekjoon_11377.py
@@ -0,0 +1,30 @@
+# pypy3
+n, m, k = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+d = [0 for _ in range(m + 1)]
+result = 0
+def dfs(s):
+    if visited[s] == True:
+        return False
+    visited[s] = True
+    for i in graph[s]:
+        if d[i] == 0 or dfs(d[i]):
+            d[i] = s
+            return True
+    return False
+for i in range(1, n + 1):
+    a = list(map(int, input().split()))
+    for j in a[1:]:
+        graph[i].append(j)
+for i in range(1, n + 1):
+    visited = [False for _ in range(n + 1)]
+    if dfs(i):
+        result += 1
+for i in range(1, n + 1):
+    visited = [False for _ in range(n + 1)]
+    if dfs(i):
+        result += 1
+        k -= 1
+        if k == 0:
+            break
+print(result)
