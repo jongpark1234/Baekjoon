3d191ba48a7ec24f61ec734b129615e815bd61db
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-03-18 09:57:48 +0900

    BOJ 수열 2
    ㆍ다이나믹 프로그래밍
    ㆍ자료 구조
    ㆍ우선순위 큐
    ㆍ함수 개형을 이용한 최적화

---
+++ 13000/baekjoon_13324.py
@@ -0,0 +1,16 @@
+import heapq
+queue = []
+n = int(input())
+a = list(map(int, input().split()))
+x = [0 for _ in range(n)]
+for i in range(n):
+    heapq.heappush(queue, (a[i] - i) * -1)
+    heapq.heappush(queue, (a[i] - i) * -1)
+    heapq.heappop(queue)
+    x[i] = heapq.heappop(queue) * -1
+    heapq.heappush(queue, -x[i])
+for i in range(n - 1)[::-1]:
+    if x[i] > x[i + 1]:
+        x[i] = x[i + 1]
+for i in range(n):
+    print(x[i] + i)
