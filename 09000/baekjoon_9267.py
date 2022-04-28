d72c8e7003c223131d57069ce46b00967a508fa3
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-04-28 09:37:02 +0900

    A+B
    ㆍ수학
    ㆍ정수론
    ㆍ확장 유클리드 호제법

--- 09000/baekjoon_9267.py
+++ 09000/baekjoon_9267.py
@@ -1,8 +1,8 @@
-def euc(a, b):
+def extendedGCD(a, b):
     if b == 0:
         return a, 1, 0
-    g, x, y = euc(b, a % b)
-    return g, y, x - (a // b) * y
+    GCD, x, y = extendedGCD(b, a % b)
+    return GCD, y, x - (a // b) * y
 def Problem(a, b, s):
     if a == b == 0:
         return s == 0
@@ -10,14 +10,13 @@ def Problem(a, b, s):
         return s % b == 0
     if b == 0:
         return s % a == 0
-    g, x, y = euc(a, b)
-    if s % g != 0:
-        return 0
-    x *= s // g
-    y *= s // g
-    for i in range(-g * x // b + 1, g * y // a + 1):
-        if euc(x + i * b // g, y - i * a // g)[0] == 1:
-            return 1
-    return 0
-a, b, s = map(int, input().split())
-print('YES' if Problem(a, b, s) else 'NO')
+    GCD, x, y = extendedGCD(a, b)
+    if s % GCD != 0:
+        return False
+    x *= s // GCD
+    y *= s // GCD
+    for i in range(-GCD * x // b + 1, GCD * y // a + 1):
+        if extendedGCD(x + i * b // GCD, y - i * a // GCD)[0] == 1:
+            return True
+    return False
+print('YES' if Problem(*map(int, input().split())) else 'NO')
