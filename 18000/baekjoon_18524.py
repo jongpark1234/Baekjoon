72d82e472d18ec1e0b1c0953a8ddea6a9c75a6d3
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-05-09 17:13:08 +0900

    Another Coin Weighing Puzzle
    ㆍ수학
    ㆍ정수론
    ㆍ포함 배제의 원리

--- 18000/baekjoon_18524.py
+++ 18000/baekjoon_18524.py
@@ -9,22 +9,22 @@ def power(x, y):
         x = x ** 2 % MOD
     return r
 result = 0
-m, n = map(int, input().split())
-low = [0 for _ in range(n + 1)]
+m, k = map(int, input().split())
+ei = [0 for _ in range(k + 1)]
 bags = [0, 1]
-for i in range(2, n + 1):
-    for j in range(i, n + 1, i):
-        if not low[j]:
-            low[j] = i
-for i in range(2, n + 1):
-    if low[i] == i:
+for i in range(2, k + 1):
+    for j in range(i, k + 1, i):
+        if not ei[j]:
+            ei[j] = i
+for i in range(2, k + 1):
+    if ei[i] == i:
         bags.append(-1)
     else:
-        if low[i // low[i]] == low[i]:
+        if ei[i // ei[i]] == ei[i]:
             bags.append(0)
         else:
-            bags.append(-bags[i // low[i]])
-for i in range(1, n + 1):
-    result += (MOD + bags[i]) * (power((n // i) * 2 + 1, m) - 1)
+            bags.append(-bags[i // ei[i]])
+for i in range(1, k + 1):
+    result += (MOD + bags[i]) * (power((k // i) * 2 + 1, m) - 1)
     result %= MOD
 print(result + 1)
