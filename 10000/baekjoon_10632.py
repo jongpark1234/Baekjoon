189c6e91774635d5fd56d01ea5272aef90593eed
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-05-24 19:40:33 +0900

    Unfair Game
    ㆍ많은 조건 분기
    ㆍ게임 이론
    ㆍ스프라그-그런디 정리

--- 10000/baekjoon_10632.py
+++ 10000/baekjoon_10632.py
@@ -1,19 +1,18 @@
 n, a, b = map(int, input().split())
-stones = [int(input()) for _ in range(n)]
-Min = min(a, b)
-grundy = 0
-heaps = 0
+piles = [int(input()) for _ in range(n)]
+minNum = min(a, b)
+k = B = N = 0
 for i in range(n):
-    grundy ^= stones[i] % (Min + 1)
-    if stones[i] > Min:
-        heaps += 1
-if heaps == 0 or a == b:
-    print(['Jiro', 'Hanako'][grundy > 0])
-elif a > b:
-    print('Hanako')
+    k ^= (piles[i] % (minNum + 1))
+    if piles[i] > minNum:
+        N = piles[i]
+        B += 1
+if B == 0 or a == b:
+    print('Hanako' if k else 'Jiro')
+elif B == 1 and minNum == a:
+    value = k ^ (N % (a + 1))
+    temp = a + 1
+    d = (((N - value) % temp) + temp) % temp
+    print('Jiro' if d <= 0 or d > a or N - d > a else 'Jiro' if value != (N - d) % temp else 'Hanako')
 else:
-    num = a + 1
-    Max = max(stones)
-    grundy = grundy ^ (Max % num)
-    r = (((Max - grundy) % num) + num) % num
-    print(['Hanako', 'Jiro'][(r == 0 or r > a or r < Max - a) or (grundy != (Max - r) % num)])
+    print('Jiro' if a < b else 'Hanako')
