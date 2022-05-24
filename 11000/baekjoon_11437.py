426df409bfcac7adb830dc87cb77b0cf1aff5f9b
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-05-24 13:31:46 +0900

    LCA
    ㆍ트리
    ㆍ최소 공통 조상

---
+++ 11000/baekjoon_11437.py
@@ -0,0 +1,49 @@
+import sys
+sys.setrecursionlimit(10**5)
+g = [[] for _ in range(101010)]
+sz = [0 for _ in range(101010)]
+par = [0 for _ in range(101010)]
+def dfs(x, p):
+    par[x] = p
+    sz[x] = 1
+    for i in g[x]:
+        if i != p:
+            sz[i] += dfs(i, x)
+    return sz[x]
+ch = [[] for _ in range(101010)]
+d = [0 for _ in range(101010)]
+chNum = [0 for _ in range(101010)]
+chIdx = [0 for _ in range(101010)]
+def hld(x, p, num, depth):
+    d[x] = depth
+    chIdx[x] = len(ch[num])
+    chNum[x] = num
+    ch[num].append(x)
+    go = -1
+    for i in g[x]:
+        if go == -1 or sz[i] > sz[go]:
+            if i != p:
+                go = i
+    if go != -1:
+        hld(go, x, num, depth)
+    for i in g[x]:
+        if i != go and i != p:
+            hld(i, x, i, depth + 1)
+def getLca(x, y):
+    while chNum[x] != chNum[y]:
+        if d[x] > d[y]:
+            x = par[chNum[x]]
+        else:
+            y = par[chNum[y]]
+    return x if chIdx[x] < chIdx[y] else y
+n = int(sys.stdin.readline())
+for nodes in range(n - 1):
+    i, j = map(int, sys.stdin.readline().split())
+    g[i].append(j)
+    g[j].append(i)
+dfs(1, 0)
+hld(1, 0, 1, 0)
+m = int(sys.stdin.readline())
+for query in range(m):
+    i, j = map(int, sys.stdin.readline().split())
+    print(getLca(i, j))
