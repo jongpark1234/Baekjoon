0fe55cb911c0a6e61b7c824580a54e7412f69b20
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-06-18 15:19:21 +0900

    Cow Gymnasts
    ㆍ수학
    ㆍ정수론
    ㆍ조합론

---
+++ 15000/baekjoon_15744.py
@@ -0,0 +1,21 @@
+MOD = 1000000007
+n = int(input())
+result = 2 - n
+divisions, m = [], []
+for i in range(1, int(n ** 0.5) + 1):
+    if n % i == 0:
+        divisions += [i, n // i]
+divisions.sort()
+divisions.pop()
+divisions = sorted(list(set(divisions)))
+m = [1] + [0 for _ in range(len(divisions) - 1)]
+for i in range(len(m)):
+    for j in range(i + 1, len(m)):
+        if divisions[j] % divisions[i] == 0:
+            m[j] -= m[i]
+for i in range(len(divisions)):
+    for j in range(i, len(divisions)):
+        if divisions[j] % divisions[i] == 0:
+            result += pow(2, divisions[j] // divisions[i], MOD) * m[i] * ((n // divisions[j] - 1) % MOD) % MOD
+            result %= MOD
+print(result)
