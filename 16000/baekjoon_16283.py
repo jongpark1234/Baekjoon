05102c73aac6f84665eb83663382101539748b59
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-08-20 20:19:50 +0900

    Farm
    ㆍ수학
    ㆍ브루트포스 알고리즘
    ㆍ사칙연산

--- 16000/baekjoon_16283.py
+++ 16000/baekjoon_16283.py
@@ -1,2 +1,10 @@
-a, b = map(int, input().split())
-print(1 / (1 + 10 ** ((b - a) / 400)))
+result = -1
+a, b, n, w = map(int, input().split())
+for i in range(1, n):
+    if a * i + b * (n - i) == w:
+        result = i
+        for j in range(i + 1, n):
+            if a * j + b * (n - j) == w:
+                print(-1)
+                exit(0)
+print(-1 if result == -1 else f'{result} {n - result}')
