becf3bdcc040c5c2665534048f241abd26bc6ab6
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-08-24 10:37:01 +0900

    씽크스몰
    ㆍ수학
    ㆍ정수론
    ㆍ고속 푸리에 변환
    ㆍ중국인의 마지막 정리

---
+++ 11000/baekjoon_11385.py
@@ -0,0 +1,17 @@
+from decimal import Decimal, Context, setcontext, MAX_PREC, MAX_EMAX
+def multiply(a, b, digit = 0):
+    setcontext(Context(prec = MAX_PREC, Emax = MAX_EMAX))
+    if digit == 0:
+        digit = len(str(min(len(a), len(b)) * max(a) * max(b)))
+    f = f'0{digit}d'
+    a_dec = Decimal(''.join(format(x, f) for x in a))
+    b_dec = Decimal(''.join(format(x, f) for x in b))
+    c_dec = a_dec * b_dec
+    total_digit = digit * (len(a) + len(b) - 1)
+    c = format(c_dec, f'0{total_digit}f')
+    return [int(c[i:i + digit]) for i in range(0, total_digit, digit)]
+result = 0
+n, m = map(int, input().split())
+for i in multiply(list(map(int, input().split())), list(map(int, input().split()))):
+    result ^= i
+print(result)
