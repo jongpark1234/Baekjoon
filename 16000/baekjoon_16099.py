1ff8ebaefcd6f000668f178f0efd2cf883c44539
JongPark <jongpark1234@naver.com>
2022-11-13 11:35:26 +0900

    Larger Sport Facility
    ㆍ수학
    ㆍ사칙연산

---
+++ 16000/baekjoon_16099.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    lt, wt, le, we = map(int, input().split())
+    if lt * wt > le * we:
+        print('TelecomParisTech')
+    elif lt * wt < le * we:
+        print('Eurecom')
+    else:
+        print('Tie')
