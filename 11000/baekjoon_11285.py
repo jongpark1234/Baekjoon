c0cbec0a91fedad4f55d2cf7441b1c607094d15d
JongPark <jongpark1234@naver.com>
2022-12-02 21:08:06 +0900

    초성 중성 종성 2

---
+++ 11000/baekjoon_11285.py
@@ -0,0 +1,17 @@
+initial = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+neutral = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+final = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' ]
+result = 44032
+a = input()
+b = input()
+c = input()
+for i in range(len(initial)):
+    if a == initial[i]:
+        result += i * 588
+for i in range(len(neutral)):
+    if b == neutral[i]:
+        result += i * 28
+for i in range(len(final)):
+    if c == final[i]:
+        result += i
+print(chr(result))
