07273c938d9377d7cefc269bfef8b0f50ab07da0
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-12-19 12:09:07 +0900

    Rectangles
    ㆍ수학
    ㆍ구현
    ㆍ사칙연산

---
+++ 09000/baekjoon_9950.py
@@ -0,0 +1,11 @@
+while True:
+    l, w, a = map(int, input().split())
+    if l == w == a == 0:
+        break
+    if l == 0:
+        l = a // w
+    elif w == 0:
+        w = a // l
+    else:
+        a = l * w
+    print(l, w, a)
