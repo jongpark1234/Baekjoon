ef78644e7ce421a26a0a14dd2aa416ebf45dccbc
JongPark <jongpark1234@naver.com>
2023-01-19 20:27:53 +0900

    PLCS
    ㆍ수학
    ㆍ다이나믹 프로그래밍
    ㆍ문자열
    ㆍ정수론
    ㆍ소수 판정
    ㆍ비트 집합

--- 25000/baekjoon_25433.cpp
+++ 25000/baekjoon_25433.cpp
@@ -1,38 +1,35 @@
 #define private public
 #include <bitset>
 #undef private
-#include <algorithm>
-#include <cmath>
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 #include <x86intrin.h>
-std::string s, t;
+using namespace std;
+string s, t;
 char x, y;
-template<std::size_t _Nw> void _M_do_sub(std::_Base_bitset<_Nw>& A, const std::_Base_bitset<_Nw>& B) {
-    for (std::size_t i = 0, c = 0; i < _Nw; i++) {
+template<size_t _Nw> void _M_do_sub(_Base_bitset<_Nw>& A, const _Base_bitset<_Nw>& B) {
+    for (size_t i = 0, c = 0; i < _Nw; i++) {
         c = _subborrow_u64(c, A._M_w[i], B._M_w[i], (unsigned long long*) & A._M_w[i]);
     }
 }
-template<> void _M_do_sub(std::_Base_bitset<1>& A, const std::_Base_bitset<1>& B) {
+template<> void _M_do_sub(_Base_bitset<1>& A, const _Base_bitset<1>& B) {
     A._M_w -= B._M_w;
 }
-template<size_t _Nb> std::bitset<_Nb>& operator-=(std::bitset<_Nb>& A, const std::bitset<_Nb>& B) {
+template<size_t _Nb> bitset<_Nb>& operator-=(bitset<_Nb>& A, const bitset<_Nb>& B) {
     return _M_do_sub(A, B), A;
 }
 
-template<size_t _Nb> inline std::bitset<_Nb> operator-(const std::bitset<_Nb>& A, const std::bitset<_Nb>& B) {
-    std::bitset<_Nb> C(A);
+template<size_t _Nb> inline bitset<_Nb> operator-(const bitset<_Nb>& A, const bitset<_Nb>& B) {
+    bitset<_Nb> C(A);
     return C -= B;
 }
 
-std::size_t Bitset(std::string s1, std::string s2) {
-    std::bitset<50000> D, Match[26];
-    for (std::size_t i = 0; i < s2.size(); i++) {
+size_t Bitset(string s1, string s2) {
+    bitset<50000> D, Match[26];
+    for (size_t i = 0; i < s2.size(); i++) {
         Match[s2[i] - 'a'][i] = 1;
     }
-    for (std::size_t i = 0; i < s1.size(); i++) {
-        auto x = Match[s1[i] - 'a'] | D;
-        auto y = D << 1;
+    for (size_t i = 0; i < s1.size(); i++) {
+        auto x = Match[s1[i] - 'a'] | D, y = D << 1;
         y[0] = 1;
         D = x ^ (x & (x - y));
     }
@@ -42,7 +39,7 @@ bool isPrime(int n) {
     if (n == 1 || n == 0) {
         return false;
     }
-    for (int i = 2; i <= static_cast<int>(std::sqrt(n)); i++) {
+    for (int i = 2; i <= static_cast<int>(sqrt(n)); i++) {
         if (n % i == 0) {
             return false;
         }
@@ -50,23 +47,15 @@ bool isPrime(int n) {
     return true;
 }
 int main() {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
-    std::cout.tie(0);
-    std::cin >> s;
-    std::cin >> t;
-    std::cin >> x;
-    std::cin >> y;
-    s.erase(std::remove(s.begin(), s.end(), y), s.end());
-    t.erase(std::remove(t.begin(), t.end(), y), t.end());
-    std::size_t pos_s = s.find(x), pos_t = t.find(x);
-    std::string s1 = s.substr(0, pos_s), s2 = s.substr(pos_s + 1);
-    std::string t1 = t.substr(0, pos_t), t2 = t.substr(pos_t + 1);
-    for (int i = static_cast<int>(Bitset(s1, t1)) + static_cast<int>(Bitset(s2, t2)) + 1; i >= 0; --i) {
+    cin >> s >> t >> x >> y;
+    s.erase(remove(s.begin(), s.end(), y), s.end());
+    t.erase(remove(t.begin(), t.end(), y), t.end());
+    size_t pos_s = s.find(x), pos_t = t.find(x);
+    for (int i = static_cast<int>(Bitset(s.substr(0, pos_s), t.substr(0, pos_t))) + static_cast<int>(Bitset(s.substr(pos_s + 1), t.substr(pos_t + 1))) + 1; i >= 0; i--) {
         if (isPrime(i)) {
-            std::cout << i << '\n';
+            cout << i << '\n';
             return 0;
         }
     }
-    std::cout << -1 << '\n';
+    cout << -1 << '\n';
 }
