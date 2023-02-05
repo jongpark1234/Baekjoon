de1b2eee9d15e76f70ffa08eeb890b9aa73008cc
JongPark <jongpark1234@naver.com>
2023-02-05 14:20:55 +0900

    Sounds fishy!
    ㆍ구현

--- 06000/baekjoon_6764.py
+++ 06000/baekjoon_6764.py
@@ -1,9 +1,12 @@
 depth = [int(input()) for _ in range(4)]
 if len(set(depth)) == 1:
     print('Fish At Constant Depth')
-elif len(set(depth)) != 4:
-    print('No Fish')
-elif depth == sorted(depth):
-    print('Fish Rising')
+elif len(set(depth)) == 4:
+    if depth == sorted(depth):
+        print('Fish Rising')
+    elif depth == sorted(depth, reverse=True):
+        print('Fish Diving')
+    else:
+        print('No Fish')
 else:
-    print('Fish Diving')
+    print('No Fish')
