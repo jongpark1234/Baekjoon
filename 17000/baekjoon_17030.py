6c2675222135001613543846dffd0f3084731fef
JongPark <jongpark1234@naver.com>
2023-02-09 21:08:03 +0900

    Cow Dating
    ㆍ수학
    ㆍ두 포인터
    ㆍ확률론

--- 17000/baekjoon_17030.py
+++ 17000/baekjoon_17030.py
@@ -2,7 +2,7 @@ result = -float('inf')
 l = r = 0
 prodValue, sumValue = 1, 0
 n = int(input())
-numlist = [int(input()) for _ in range(n)]
+numlist = [int(input()) * 0.000001 for _ in range(n)]
 while l < n:
     while r < n and sumValue < 1:
         prodValue *= 1 - numlist[r]
@@ -12,4 +12,4 @@ while l < n:
     prodValue /= 1 - numlist[l]
     sumValue -= numlist[l] / (1 - numlist[l])
     l += 1
-print(int(result))
+print(int(result * 1000000))
