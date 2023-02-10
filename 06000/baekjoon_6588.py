fa33d763fae9e70b1ac515e56e7c958f3045f396
JongPark <jongpark1234@naver.com>
2023-02-10 16:15:13 +0900

    골드바흐의 추측
    ㆍ수학
    ㆍ정수론
    ㆍ소수 판정
    ㆍ에라토스테네스의 체

--- 06000/baekjoon_6588.py
+++ 06000/baekjoon_6588.py
@@ -1,18 +1,18 @@
 from sys import stdin
-from math import sqrt
-def isprime(num):
-    if num == 1:
-        return False
-    else:
-        for i in range(2, int(sqrt(num) + 1)):
-            if num % i == 0:
-                return False
-        return True
+from math import isqrt
+MAX = 1000000
+sieve = [False, False] + [True for _ in range(MAX - 1)]
+for i in range(2, isqrt(MAX) + 1):
+    if sieve[i]:
+        for j in range(i * 2, MAX, i):
+            sieve[j] = False
 while True:
     n = int(stdin.readline())
     if n == 0:
         break
-    for i in range(3, n):
-        if isprime(i) and (isprime(n - i)):
+    for i in range(3, MAX + 1, 2):
+        if sieve[i] and sieve[n - i]:
             print(f'{n} = {i} + {n - i}')
             break
+    else:
+        print('Goldbach\'s conjecture is wrong.')
