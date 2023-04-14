76bf22effb5f500f210f8acb33163d0945fc859b
JongPark <jongpark1234@naver.com>
2023-04-14 11:45:54 +0900

    피보나치 인버스
    ㆍ이분 탐색
    ㆍ임의 정밀도 / 큰 수 연산

--- 10000/baekjoon_10425.py
+++ 10000/baekjoon_10425.py
@@ -1,8 +1,7 @@
 fiboMap = { 0: 0, 1: 1 }
 a, b = 0, 1
 for i in range(2, 100001):
-    temp = a + b
-    fiboMap[temp] = i
-    a, b = b, temp
+    a, b = b, a + b
+    fiboMap[b] = i
 for i in [*open(0)][1:]:
     print(fiboMap[int(i)])
