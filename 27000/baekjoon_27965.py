198b3b8fa1184afd1e796c59731798d016ea848c
JongPark <jongpark1234@naver.com>
2023-04-28 12:19:10 +0900

    N결수
    ㆍ수학
    ㆍ정수론

--- 27000/baekjoon_27965.py
+++ 27000/baekjoon_27965.py
@@ -1,6 +1,6 @@
 from math import log10
-n, k = map(int, input().split())
 result = 0
+n, k = map(int, input().split())
 for i in range(1, n + 1):
     result = (result * 10 ** int(log10(i) + 1) + i) % k
 print(result)
