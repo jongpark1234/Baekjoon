b8346050e4c5730411a91336a8edf30d7eb9a09a
JongPark <jongpark1234@naver.com>
2023-06-17 15:18:05 +0900

    Quadratics
    ㆍ수학
    ㆍ사칙연산

--- 26000/baekjoon_26548.py
+++ 26000/baekjoon_26548.py
@@ -1,5 +1,4 @@
 from decimal import Decimal
-from math import sqrt
 def f(pm: Decimal) -> Decimal:
     return round((-b + (b ** 2 - Decimal(4) * a * c) ** Decimal(0.5) * pm) / (Decimal(2) * a), 3)
 for _ in range(int(input())):
