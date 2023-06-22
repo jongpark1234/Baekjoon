ab98f3f13840a71161fcde920a8f87970be123e3
JongPark <jongpark1234@naver.com>
2023-06-22 21:00:47 +0900

    터렛
    ㆍ수학
    ㆍ기하학
    ㆍ많은 조건 분기

--- 01000/baekjoon_1002.py
+++ 01000/baekjoon_1002.py
@@ -1,11 +1,12 @@
-import sys
-import math
-t = int(sys.stdin.readline())
-for _ in range(t):
-    x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split())
-    distance = math.sqrt(pow(abs(x1 - x2), 2) + pow(abs(y1 - y2), 2))
+from sys import stdin
+from math import hypot
+for _ in range(int(input())):
+    x1, y1, r1, x2, y2, r2 = map(int, stdin.readline().split())
+    distance = hypot(abs(x1 - x2), abs(y1 - y2))
     outrdis, inrdis = r1 + r2, abs(r1 - r2)
-    if distance == 0 and r1 == r2:
+    if x1 == y1 == r1 == x2 == y2 == r2 == 0:
+        print(1)
+    elif distance == 0 and r1 == r2:
         print(-1)
     elif inrdis == distance or outrdis == distance:
         print(1)
