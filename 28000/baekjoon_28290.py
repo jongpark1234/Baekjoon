c47d06021aba99f64abe1a5edca86aef9dd56bc1
JongPark <jongpark1234@naver.com>
2023-07-07 19:24:11 +0900

    안밖? 밖안? 계단? 역계단?
    ㆍ구현

---
+++ 28000/baekjoon_28290.py
@@ -0,0 +1,11 @@
+s = input()
+if s in ['fdsajkl;', 'jkl;fdsa']:
+    print('in-out')
+elif s in ['asdf;lkj', ';lkjasdf']:
+    print('out-in')
+elif s == 'asdfjkl;':
+    print('stairs')
+elif s == ';lkjfdsa':
+    print('reverse')
+else:
+    print('molu')
