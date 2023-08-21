26f41c418be05f7551cda87538f4285c9f479c11
JongPark <jongpark1234@naver.com>
2023-08-21 18:16:20 +0900

    BOJ 수열 1
    ㆍ다이나믹 프로그래밍
    ㆍ자료 구조
    ㆍ우선순위 큐
    ㆍ함수 개형을 이용한 최적화

--- 13000/baekjoon_13323.py
+++ 13000/baekjoon_13323.py
@@ -1,18 +1,14 @@
 import heapq
-Min = 0
-queue = []
+pq, x = [], []
 n = int(input())
 a = list(map(int, input().split()))
-x = [0 for _ in range(n)]
 for i in range(n):
-    heapq.heappush(queue, (a[i] - i) * -1)
-    heapq.heappush(queue, (a[i] - i) * -1)
-    heapq.heappop(queue)
-    x[i] = heapq.heappop(queue) * -1
-    heapq.heappush(queue, -x[i])
-for i in range(n - 1)[::-1]:
+    for _ in range(2):
+        heapq.heappush(pq, -(a[i] - i))
+    heapq.heappop(pq)
+    x.append(-heapq.heappop(pq))
+    heapq.heappush(pq, -x[i])
+for i in range(n - 2, -1, -1):
     if x[i] > x[i + 1]:
         x[i] = x[i + 1]
-for i in range(n):
-    Min += abs(a[i] - x[i] - i)
-print(Min)
+print(sum(abs(a[i] - x[i] - i) for i in range(n)))
