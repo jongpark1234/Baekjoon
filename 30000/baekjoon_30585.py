bf3931417caf5f7e0785041d492aca6cbee6b092
JongPark <jongpark1234@naver.com>
2023-11-07 19:14:31 +0900

    Поп-ит
    ㆍ구현
    ㆍ문자열

--- 30000/baekjoon_30585.py
+++ 30000/baekjoon_30585.py
@@ -3,5 +3,5 @@ h, w = map(int, input().split())
 for i in range(h):
     s = input()
     for j in range(2):
-        result[j] += .count(str(j))
+        result[j] += w.count(str(j))
 print(min(result[0], result[1], h * w - result[0], h * w - result[1]))
