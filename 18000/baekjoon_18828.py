fc096736fcea4ba0778394b69a79d3aa42ba00e6
JongPark <jongpark1234@naver.com>
2023-11-28 01:10:30 +0900

    순서쌍
    ㆍ수학
    ㆍ정수론

--- 18000/baekjoon_18828.py
+++ 18000/baekjoon_18828.py
@@ -1,9 +1,7 @@
 n = int(input())
-ret, num = 0, 1
-while num <= int(n ** 0.5):
-    if n % num == 0:
-        ret += 2
-    num += 1
-if (num - 1) ** 2 == n:
-    ret -= 1
-print(ret)
+result, temp = 0, 1
+while temp <= int(n ** 0.5):
+    if n % temp == 0:
+        result += 2
+    temp += 1
+print(result - ((temp - 1) ** 2 == n))
